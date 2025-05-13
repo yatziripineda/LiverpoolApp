ac56b0030c4206005939801f1f8e557b5904a0ab
yatziripineda <131062655+yatziripineda@users.noreply.github.com>
2025-05-13 13:33:41 -0600

    1

---
+++ LIVERPOOLApp/LIVERPOOLApp/LIVERPOOLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LIVERPOOLAppApp.swift
+//  LIVERPOOLApp
+//
+//  Created by yatziri on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LIVERPOOLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
